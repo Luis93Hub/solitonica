4db18f0f4f905bfede6eccfa2deaae99d81a7bb3
Luis Hernández <luis93hernan2@gmail.com>
2025-09-14 17:44:11 +0300

    Fix plugin next-intl

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,7 +3,7 @@ import { getRequestConfig } from "next-intl/server";
 export default getRequestConfig(async()=>{
   const locale = 'es-NI';
 
-  const messages = (await import(`../messages/${locale}.json`)).default;
+  const messages = (await import(`./messages/${locale}.json`)).default;
 
   return {
     locale,
